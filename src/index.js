f91e4e062ca4079ce43a47046dc9bf783ec561ef
noorahmad <noorahmad23@hotmail.com>
2017-07-25 15:04:11 -0700

    created search bar and made it into controlled component, added youtube api call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar'
+
+const API_KEY = "AIzaSyBoUQl4qB5ouihVT2egLOrFyGGVwVd0bdY"
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    //initialize an array to hold videos
+    this.state={ videos: [] }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      //API call that gathers data from surfboards search term. videos = data
+      this.setState({ videos })
+      // this.setState({ videos: videos })
+      // first videos from array, second one from api call
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
